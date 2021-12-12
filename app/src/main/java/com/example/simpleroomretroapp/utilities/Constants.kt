7fb9ee7266ffae6d12262789f5d34556e923cb3b
Igor Dmitriev <igorek1955@gmail.com>
2021-12-12 19:50:39 +0300

    db name fix

--- app/src/main/java/com/example/simpleroomretroapp/utilities/Constants.kt
+++ app/src/main/java/com/example/simpleroomretroapp/utilities/Constants.kt
@@ -3,5 +3,5 @@ package com.example.simpleroomretroapp.utilities
 object Constants {
     const val BASE_URL = "https://reqres.in"
     const val USERS_TABLE = "users_table"
-    const val USERS_DATABASE_NAME = "users_table"
+    const val USERS_DATABASE_NAME = "users_database"
 }
